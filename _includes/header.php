14ba55150e64a3e054e62f24de05498fcdaf5053
Josh Sanger <joshsanger88@gmail.com>
2017-05-29 22:27:42 -0400

    Changed title in header.

--- _includes/header.php
+++ _includes/header.php
@@ -4,7 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
-        <title>ENTER TITLE</title>
+        <title>Css Only Slider</title>
         <script src="<?php echo BASE_URL;?>/_assets/js/lib/modernizr.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
