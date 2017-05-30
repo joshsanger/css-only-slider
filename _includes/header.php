548c237c91a4dc5c1096eb8a97e0329e417481a6
Josh S <josh.sanger@geekpower.ca>
2017-05-30 17:29:28 -0400

    Added in favicons reference.

--- _includes/header.php
+++ _includes/header.php
@@ -8,6 +8,16 @@
         <script src="<?php echo BASE_URL;?>/_assets/js/lib/modernizr.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo HOME_URL?>/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo HOME_URL?>/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo HOME_URL?>/favicons/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo HOME_URL?>/favicons/manifest.json">
+        <link rel="mask-icon" href="<?php echo HOME_URL?>/favicons/safari-pinned-tab.svg" color="#4f8bb6">
+        <link rel="shortcut icon" href="<?php echo HOME_URL?>/favicons/favicon.ico">
+        <meta name="msapplication-config" content="<?php echo HOME_URL?>/favicons/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+
         <link rel="stylesheet" href="<?php echo BASE_URL;?>/_assets/css/master.min.css">
 
     </head>
