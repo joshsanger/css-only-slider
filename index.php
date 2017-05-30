6a350690e19fe7a0f62e018e9c282c90780f84b2
Josh Sanger <joshsanger88@gmail.com>
2017-05-29 21:25:05 -0400

    Finished the responsive styling.

--- index.php
+++ index.php
@@ -9,17 +9,23 @@
     <input type="radio" name="slide-control" id="control1" checked/>
     <label for="control1"></label>
     <div class="slide" style="background-image: url(https://unsplash.it/1200/600/?image=1062)">
-        <span>Slide 1</span>
+        <div>
+            <span>Slide 1</span>
+        </div>
     </div>
     <input type="radio" name="slide-control" id="control2"/>
     <label for="control2"></label>
     <div class="slide" style="background-image: url(https://unsplash.it/1200/600/?image=876)">
-        <span>Slide 2</span>
+        <div>
+            <span>Slide 2</span>
+        </div>
     </div>
     <input type="radio" name="slide-control" id="control3"/>
     <label for="control3"></label>
     <div class="slide" style="background-image: url(https://unsplash.it/1200/600/?image=957)">
-        <span>Slide 3</span>
+        <div>
+            <span>Slide 3</span>
+        </div>
     </div>
 </div>
 
