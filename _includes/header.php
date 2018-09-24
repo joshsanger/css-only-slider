5d4708cb1143370667da016aee262b21db395555
Josh S <josh.sanger@geekpower.ca>
2018-09-23 22:03:35 -0400

    Changed title and added tag

--- _includes/header.php
+++ _includes/header.php
@@ -4,7 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
-        <title>Css Only Slider</title>
+        <title>Css Only Sliding Showcase</title>
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo BASE_URL;?>/_assets/css/master.min.css">
